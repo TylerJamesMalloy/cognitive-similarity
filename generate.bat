9d06e153af7e95fcfcf0f12807bab21598f992c5
Tyler Malloy <tylerjmalloy@gmail.com>
2024-10-02 16:55:39 -0400

    adding new color scheme to figures

--- generate.bat
+++ generate.bat
@@ -1,13 +1,20 @@
-python .\generateSims.py -s human,semantic -ofp ./Results/Figure1.pkl
-python .\generateSims.py -s human,cosine -ofp ./Results/Figure2.pkl
-python .\generateSims.py -s human,weighted -ofp ./Results/Figure3.pkl
-python .\generateSims.py -s human,pruned  -ofp ./Results/Figure4.pkl
-python .\generateSims.py -s human,ensemble -ofp ./Results/Figure5.pkl
-python .\generateSims.py -s human,ibis -ofp ./Results/Figure6.pkl
+:: python .\generateSims.py -s human,human -ofp ./Results/Figure0.pkl
+:: python .\generateSims.py -s human,semantic -ofp ./Results/Figure1.pkl
+:: python .\generateSims.py -s human,cosine -ofp ./Results/Figure2.pkl
+:: python .\generateSims.py -s human,weighted -ofp ./Results/Figure3.pkl
+:: python .\generateSims.py -s human,pruned  -ofp ./Results/Figure4.pkl
+:: python .\generateSims.py -s human,ensemble -ofp ./Results/Figure5.pkl
+:: python .\generateSims.py -s human,ibis -ofp ./Results/Figure6.pkl
+python .\generateSims.py -s human,pruned -ofp ./Results/Figure7a.pkl -i
+python .\generateSims.py -s human,ensemble -ofp ./Results/Figure7b.pkl -i 
+python .\generateSims.py -s human,ibis -ofp ./Results/Figure7c.pkl -i
 
+python .\generatePlots.py  -dp ./Results/Figure0.pkl -ofp ./Figures/Figure0.png -ll center-right
 python .\generatePlots.py  -dp ./Results/Figure1.pkl -ofp ./Figures/Figure1.png -ll center-right
 python .\generatePlots.py  -dp ./Results/Figure2.pkl -ofp ./Figures/Figure2.png -ll center-right
 python .\generatePlots.py  -dp ./Results/Figure3.pkl -ofp ./Figures/Figure3.png
 python .\generatePlots.py  -dp ./Results/Figure4.pkl -ofp ./Figures/Figure4.png
 python .\generatePlots.py  -dp ./Results/Figure5.pkl -ofp ./Figures/Figure5.png
 python .\generatePlots.py  -dp ./Results/Figure6.pkl -ofp ./Figures/Figure6.png
+
+python .\generatePlots.py  -dp ./Results/Figure7a.pkl -ofp ./Figures/Figure7a.png 
--- temp.py
+++ temp.py
@@ -76,8 +76,6 @@ if __name__ == '__main__':
                     help='Path to save the png output file to.')
     parser.add_argument('-ll', '--legendLoc', dest='legendLoc', type=str,  default="upper-right",
                     help='Location to plot the legend. Options are upper-right, lower-right, center-right, and so on.') 
-    parser.add_argument('-p', '--participant', dest='participant', type=str,  default='all',
-                    help='Participants to show, options are "all" which shows all, "random" which picks 1 random, or a string representing a list of participant IDs to show (e.g [1] or [2,47], etc).')
 
     # Classification arguments
     parser.add_argument('-c', '--classify', dest='classify', type=str,  default="human",
@@ -87,9 +85,6 @@ if __name__ == '__main__':
 
     df = pd.read_pickle(args.dataPath)
     args.legendLoc = " ".join(args.legendLoc.split("-"))
-    if(args.participant != 'all'):
-        if(args.participant == 'random'):
-            id = np.random(0,len(df['UserId'].unique()))
 
     # list of (estimator, param_grid), where param_grid is used in GridSearchCV
     # The parameter spaces in this example are limited to a narrow band to reduce
@@ -134,125 +129,103 @@ if __name__ == '__main__':
         classifier_similarities = np.array(classifier_similarities).T
         classifiers_similarities.append(classifier_similarities)
     
-    # Dark Blue, Dark Orange, Dark Red, Dark Purple, Dark Brown, Dark Pink, Dark Grey, Dark Yellow
-    # Light Blue, Light Orange, Light Red, Light Purple, Light Brown, Light Pink, Light Grey, Light Yellow
-    colors = sns.color_palette("dark")[0:len(classifiers)+1]
-    light_colors = sns.color_palette("pastel")[0:len(classifiers)+1]
-    
+    # Currently can only plot if there are 2 document annotation categories
+    if(len(categories) == 2):
+        for (classifier, classifier_df, classifier_similarity) in zip(classifiers, classifiers_dfs, classifiers_similarities):
+            #palette = [(0.267, 0.675, 0.761), (0.929, 0.627, 0.322), (0.2980392156862745, 0.4470588235294118, 0.6901960784313725), (0.8666666666666667, 0.5176470588235295, 0.3215686274509804)]
+            palette = [(0.267, 0.675, 0.761), (0.929, 0.627, 0.322), (0.2980392156862745, 0.4470588235294118, 0.6901960784313725), (0.8666666666666667, 0.5176470588235295, 0.3215686274509804), ]
+            g = sns.jointplot(data=df, x=categories[0], y=categories[1], palette=palette, hue="Type", xlim = (-0.1,1.1), ylim = (-0.1,1.1)).plot_joint(sns.kdeplot, zorder=5, n_levels=5)
+                #sns.move_legend(g.figure, "lower left")
+            
+            n_samples = 100
 
-    if(len(list(df['Similarity Metric'].unique())) == 1):
-        hue_order = ['phishing human', 'ham human']
-        palette = sns.color_palette(colors)
-
-        cm_bright = ListedColormap([palette[1], palette[0]]) 
-        cm_brighter = ListedColormap([palette[1], palette[0]]) 
-    else:
-        hue_order = ['phishing human', 'phishing ' + classifier, 'ham human', 'ham ' + classifier]
-        palette = []
-        for x in range(len(classifiers)+1):
-            palette.append(colors[x])
-            palette.append(light_colors[x])
-        
-        palette = sns.color_palette(palette)
-
-        cm_bright = ListedColormap([palette[2], palette[0]]) 
-        cm_brighter = ListedColormap([palette[3], palette[1]])
-
-    for (classifier, classifier_df, classifier_similarity) in zip(classifiers, classifiers_dfs, classifiers_similarities):
-        g = sns.jointplot(data=df, x=categories[0], y=categories[1], hue_order=hue_order, palette=palette, hue="Type", xlim = (-0.1,1.1), ylim = (-0.1,1.1)).plot_joint(sns.kdeplot, zorder=5, n_levels=5, hue_order=hue_order)
-            #sns.move_legend(g.figure, "lower left")
-        n_samples = 100
-
-        if(len(list(df['Similarity Metric'].unique())) > 1):
-            g.figure.suptitle("Human Participant and " + classifier.capitalize() + " Similarity \n of Phishing and Ham Emails", fontsize=18)
-        else:
             g.figure.suptitle("Human Participant Similarity \n of Phishing and Ham Emails", fontsize=18)
+            g.ax_joint.collections[0].set_alpha(0.9)
+            g.figure.tight_layout()
+            g.figure.subplots_adjust(top=0.95) # Reduce plot to make room
 
-        g.ax_joint.collections[0].set_alpha(0.9)
-        g.figure.tight_layout()
-        g.figure.subplots_adjust(top=0.95) # Reduce plot to make room
+            g.ax_joint.set_xlabel("Human Ham Similarity", fontsize=16)
+            g.ax_joint.set_ylabel("Human Phishing Similarity", fontsize=16)
 
-        g.ax_joint.set_xlabel("Human Ham Similarity", fontsize=16)
-        g.ax_joint.set_ylabel("Human Phishing Similarity", fontsize=16)
+            X = classify_similarities
+            y = classify_df["Type"].to_numpy() == 'phishing human'
+            if(len(classifiers) == 0): # Use a train/test split of classify data for graph
+                X_train, X_test, y_train, y_test = train_test_split(
+                    X, y, test_size=0.5, random_state=42
+                )
+            else: # Use the classifier similarity metric as the train data, and classify similarities as the test
+                X_test = X
+                y_test = y
+                X_train = classifier_similarity
+                y_train = classifier_df["Type"].to_numpy() == 'phishing ' + classifier
+            
+            cm_piyg = sns.diverging_palette(20,220, as_cmap=True)
+            cm_bright = ListedColormap([(0.8666666666666667, 0.5176470588235295, 0.3215686274509804), (0.2980392156862745, 0.4470588235294118, 0.6901960784313725)])
+            cm_brighter = ListedColormap([(0.929, 0.627, 0.322), (0.267, 0.675, 0.761)])
 
-        X = classify_similarities
-        y = classify_df["Type"].to_numpy() == 'phishing human'
-        if(len(classifiers) == 0): # Use a train/test split of classify data for graph
-            X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=0.5, random_state=42
-            )
-        else: # Use the classifier similarity metric as the train data, and classify similarities as the test
-            X_test = X
-            y_test = y
-            X_train = classifier_similarity
-            y_train = classifier_df["Type"].to_numpy() == 'phishing ' + classifier
+            # create the grid for background colors
+            x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+            y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+            xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
         
-        cm_piyg = sns.diverging_palette(20,220, as_cmap=True) 
+            # plot the dataset first
+            ax = g.ax_joint
+            # plot the training points
+            ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_brighter, edgecolors="k")
+            # and testing points
+            ax.scatter(
+                X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6, edgecolors="k"
+            )
+            ax.set_xlim(-0.1,1.1)
+            ax.set_ylim(-0.1,1.1)
+            #ax.set_xticks(())
+            #ax.set_yticks(())
 
-        # create the grid for background colors
-        x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
-        y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
-    
-        # plot the dataset first
-        ax = g.ax_joint
-        # plot the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_brighter, alpha=0.5)
-        # and testing points
-        ax.scatter(
-            X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.5
-        )
-        ax.set_xlim(-0.1,1.1)
-        ax.set_ylim(-0.1,1.1)
-        #ax.set_xticks(())
-        #ax.set_yticks(())
+            # compute KL divergence of category KDEs
+            from sklearn.neighbors import KernelDensity
+            kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(X_train[y_train])
+            score = kde.score(X_test[y_test])
 
-        # compute KL divergence of category KDEs
-        from sklearn.neighbors import KernelDensity
-        kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(X_train[y_train])
-        score = kde.score(X_test[y_test])
+            kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(X_train[y_train == 0])
+            score += kde.score(X_test[y_test == 0])
 
-        kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(X_train[y_train == 0])
-        score += kde.score(X_test[y_test == 0])
+            ax = g.ax_joint
+            estimator = regressions[0][0] 
+            param_grid = regressions[0][1] 
+            clf = GridSearchCV(estimator=estimator, param_grid=param_grid)
+            with ignore_warnings(category=ConvergenceWarning):
+                clf.fit(X_train, y_train)
+            score1 = clf.score(X_test, y_test)
 
-        ax = g.ax_joint
-        estimator = regressions[0][0] 
-        param_grid = regressions[0][1] 
-        clf = GridSearchCV(estimator=estimator, param_grid=param_grid)
-        with ignore_warnings(category=ConvergenceWarning):
-            clf.fit(X_train, y_train)
-        score1 = clf.score(X_test, y_test)
+            # plot the decision boundary. For that, we will assign a color to each
+            # point in the mesh [x_min, x_max]*[y_min, y_max].
+            if hasattr(clf, "decision_function"):
+                Z = clf.decision_function(np.column_stack([xx.ravel(), yy.ravel()]))
+            else:
+                Z = clf.predict_proba(np.column_stack([xx.ravel(), yy.ravel()]))[:, 1]
 
-        # plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]*[y_min, y_max].
-        if hasattr(clf, "decision_function"):
-            Z = clf.decision_function(np.column_stack([xx.ravel(), yy.ravel()]))
-        else:
-            Z = clf.predict_proba(np.column_stack([xx.ravel(), yy.ravel()]))[:, 1]
+            # put the result into a color plot
+            Z = Z.reshape(xx.shape)
+            ax.contourf(xx, yy, Z, cmap=cm_piyg, alpha=0.8)
 
-        # put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z, cmap=cm_piyg, alpha=0.8)
+            # plot the training points
+            ax.scatter(
+                X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_brighter, edgecolors="k"
+            )
+            # and testing points
+            ax.scatter(
+                X_test[:, 0],
+                X_test[:, 1],
+                c=y_test,
+                cmap=cm_bright,
+                edgecolors="k",
+                alpha=0.6,
+            )
+            ax.set_xlim(-0.1,1.1)
+            ax.set_ylim(-0.1,1.1)
+            #ax.set_xticks(())
+            #ax.set_yticks(())
 
-        # plot the training points
-        ax.scatter(
-            X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_brighter, edgecolors="k"
-        )
-        # and testing points
-        ax.scatter(
-            X_test[:, 0],
-            X_test[:, 1],
-            c=y_test,
-            cmap=cm_bright,
-            edgecolors="k",
-            alpha=0.6,
-        )
-        ax.set_xlim(-0.1,1.1)
-        ax.set_ylim(-0.1,1.1)
-        #ax.set_xticks(())
-        #ax.set_yticks(())
-
-        if(len(list(df['Similarity Metric'].unique())) != 1):
             ax.text(
                 0.95,
                 0.06,
@@ -264,12 +237,12 @@ if __name__ == '__main__':
             )
 
 
-        plt.tight_layout()
-        #plt.legend(loc='lower left')
-        plt.legend(loc=args.legendLoc)
+            plt.tight_layout()
+            #plt.legend(loc='lower left')
+            plt.legend(loc=args.legendLoc)
 
-        if(args.show):
-            plt.show()
+            if(args.show):
+                plt.show()
 
-        fig = g.figure
-        fig.savefig(args.outPath) 
+            fig = g.figure
+            fig.savefig(args.outPath) 
